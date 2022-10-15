a92100b45c5daaf889a782359ad3f583264d0bf5
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-10-15 08:49:39 +0700

    :rotating_light: update Site Head

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Hi, I'm Vatanak. I'm a web developer based in the city of Phnom Penh, Cambodia. I'm a
+        passionate about creating beautiful and functional websites."
+        />
+        <title>Vatanak Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
