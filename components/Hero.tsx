3999af685154e7ae1aae53b42686fed883695dd0
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-10-15 13:19:30 +0700

    :art: hero section drafting

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,27 @@
 export function Hero() {
   return (
     <header
-      class="section sec1 header active min-h-screen w-full py-12 px-72"
+      class="section sec1 header active min-h-screen w-full"
       id="hero"
     >
       <div class="header-content grid grid-cols-2 min-h-screen">
         <div class="left-header flex items-center relative">
-          <div class="h-shape h-full w-4/6" />
-          <div class="image">
-            <img src="img/profile.webp" alt="" />
+          <div
+            class="h-shape h-full w-4/6 bg-indigo-700 absolute left-0 top-0"
+            style={{
+              clipPath: "polygon(0 0, 46% 0, 79% 100%, 0 100%)",
+              zIndex: -1,
+            }}
+          />
+          <div
+            class="image rounded-2xl ml-16 bg-gray-800 transition-all duration-300 ease-in-out"
+            style={{ height: "90%", width: "70%" }}
+          >
+            <img
+              src="/profile.webp"
+              alt="A guy in a suit"
+              className="w-full h-full object-cover transition-all duration-300 ease-in-out"
+            />
           </div>
         </div>
         <div class="right-header">
--- twind.config.ts
+++ twind.config.ts
@@ -1,13 +1,4 @@
 import { Options } from "$fresh/plugins/twind.ts";
-import { setup } from "twind";
-
-setup({
-  theme: {
-    colors: {
-      primary: "#0b486b",
-    },
-  },
-});
 
 export default {
   selfURL: import.meta.url,
